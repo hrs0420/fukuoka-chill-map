8472113741c62b77692ec73ceeff088bf42517be
hrs0420 <takahiroshi0420@gmail.com>
2026-07-31 11:01:33 +0900

    Add cafe search feature

--- js/script.js
+++ js/script.js
@@ -1,20 +1,43 @@
 const searchInput = document.getElementById("search");
+const list = document.getElementById("cafe-list");
 
+
+let cafes = [];
+
+
+//カフェ一覧を表示する関数
+function displayCafes(cafeData) {
+    list.innerHTML = "";
+
+    cafeData.forEach(cafe => {
+        list.innerHTML +=
+            <div class="cards">
+                <h2>${cafe.name}</h2>
+                <p>📍 ${cafe.area}</p>
+                <p>⭐ ${cafe.rating}</p>
+                <p>${cafe.description}</p>
+            </div>
+        ;
+    });
+}
+
+//JSONを読み込む
 fetch("data/cafes.json")
     .then(response => response.json())
-    .then(cafes => {
-
-        const list = document.getElementById("cafe-list");
-
-        cafes.forEach(cafe => {
-
-            list.innerHTML +=`
-                <div class="card">
-                    <h2>${cafe.name}</h2>
-                    <p>📍 ${cafe.area}</p>
-                    <p>⭐ ${cafe.rating}</p>
-                    <p>${cafe.description}</p>
-                </div>
-           `;
-        });
+    .then(data =>{
+        cafes = data;
+        displayCafes(cafes);
     });
+
+//検索機能
+searchInput.addEventListener("input", () =>{
+
+    const keyword = searchInput.ariaValueMax.toLowerCase();
+
+    const filtered = cafes.filter(cafe =>
+        cafe.name.toLowerCase().includes(keyword)
+    );
+
+    displayCafes(filtered);
+
+});
